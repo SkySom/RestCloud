24bc8e9d63af292bd8d90d512402e38bf82477da
SkySom <skylar.som@gmail.com>
2014-02-06 21:58:41 -0500

    Added some Calendar stuff, removed tests/public
    Begin to add the things needs for Calendars. The tests were the default
    test and thus were rendered useless when I deleted Application.scala.
    The public stuff isn't needed since this is restful. Still got the
    images folder. Maybe be useful. Probably not.

---
+++ app/controllers/CalendarController.scala
@@ -0,0 +1,8 @@
+package controllers
+
+/**
+ * Created by Skylar on 2/6/14.
+ */
+class CalendarController {
+
+}
---
+++ app/models/Calendar.scala
@@ -0,0 +1,8 @@
+package models
+
+/**
+ * Created by Skylar on 2/6/14.
+ */
+class Calendar(name : String, description : String) {
+
+}
