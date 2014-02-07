3135f603ede8d5211d375314f69b0dc28c6bcbe6
SkySom <skylar.som@gmail.com>
2014-02-06 22:53:49 -0500

    Removed RUNNING_PID
    Also remove routes comment and added a dummy list method

--- app/controllers/CalendarController.scala
+++ app/controllers/CalendarController.scala
@@ -1,8 +1,13 @@
 package controllers
 
+import play.mvc.Controller
+import play.api.mvc.Action
+import play.api.mvc.Results._
 /**
  * Created by Skylar on 2/6/14.
  */
-class CalendarController {
-
+object CalendarController extends Controller{
+	def List = Action {
+		request => Ok("Got request [" + request + "]")
+	}
 }
