1757c065717bbe81def7df76bc019cacf016de9a
SkySom <skylar.som@gmail.com>
2014-02-11 17:00:22 -0500

    CalendarController.Create now returns Json (Meaningless for the most part) and CalendarSpec has been added for testing with What Create currently does. Will need to switch when create is finished.

--- app/controllers/CalendarController.scala
+++ app/controllers/CalendarController.scala
@@ -8,17 +8,31 @@ package controllers
 import play.mvc.Controller
 import play.api.mvc.Action
 import play.api.mvc.Results._
-import play.api.libs.json
 import models.Calendar
-import play.api.libs.json.{Json, JsValue}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 object CalendarController extends Controller {
+
+	implicit val rds =
+		(__ \ 'name).read[String] and
+		(__ \ 'description).read[String]
+
 	def List = Action { request =>
 		Ok("Got request [" + request + "]")
 	}
 
 	def Create = Action { request =>
-		Ok("Here's the body ")
+		request.body.asJson.map { json =>
+			json.validate[(String, String)].map{
+				case (name, description) => Ok(Json.obj("status" -> "OK", "message" -> Json.obj("name" -> name,
+					"description" -> description)))
+			}.recoverTotal{
+				e => Status(400)(Json.obj("status" -> "KO", "message" -> JsError.toFlatJson(e)))
+			}
+		}.getOrElse {
+			Status(400)(Json.obj("status" -> "KO", "message" -> "Expecting Json Data"))
+		}
 	}
 
 	def Delete(id: Int) = Action { request =>
