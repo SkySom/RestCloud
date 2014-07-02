ffabc311b18ad02d9f4097758fa283080ef50001
SkySom <skylar.som@gmail.com>
2014-07-02 16:06:21 -0400

    Moved from config.yaml to config.json

--- source/configuration/appConfig.d
+++ source/configuration/appConfig.d
@@ -1,11 +1,14 @@
 module restcloud.configuration.appConfig;
 
 import restcloud.configuration.baseConfig;
+import vibe.data.json;
+
 
 class AppConfigUtil : BaseConfigUtil {
 
 }
 
 struct AppConfig {
-
+  int port;
+  string address;
 }
