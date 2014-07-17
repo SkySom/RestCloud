4a104f97e5b972bb18da977df4899139597b960c
SkySom <skylar.som@gmail.com>
2014-07-17 13:47:05 -0400

    Updated dub.json to new format.

--- source/app.d
+++ source/app.d
@@ -1,5 +1,8 @@
 import std.stdio;
+
 import vibe.d;
+//import ddbc.drivers.pgsqlddbc;
+
 import restcloud.configuration.appconfig;
 
 shared static this()
@@ -9,8 +12,9 @@ shared static this()
 	settings.port = appConfig.port;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
 	listenHTTP(settings, &hello);
-
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	//PGSQLDriver driver = new PGSQLDriver();
+	logInfo("Please open http://%s:%d/in your broswer.",appConfig.address,
+	 appConfig.port);
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res)
